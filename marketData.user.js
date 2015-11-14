6f065b50d518cba79e6d54678eb999871c7998be
badbyte <macm241192@gmail.com>
2015-11-14 18:34:13 +0100

    iter4

--- marketData.user.js
+++ marketData.user.js
@@ -10,33 +10,43 @@
 // @downloadURL  https://raw.githubusercontent.com/miguel200761/csgoMarketCheck/master/marketData.js
 // ==/UserScript==
 
-var inspectsClass = document.getElementsByClassName("sih-market-action");
-var pricesClass = document.getElementsByClassName("market_listing_price market_listing_price_with_fee");
-var inspects = new Array(inspectsClass.length);
-var prices = new Array(pricesClass.length);
+//Create Button
+var button = document.createElement('BUTTON');
+button.setAttribute('ONCLICK', 'start()');
+var t = document.createTextNode("List!"); 
+button.appendChild(t);
+document.body.appendChild(button);
 
-var data = new Array(inspectsClass.length);
 
-//Get Values
-for (i = 0; i < inspectsClass.length; i++) { 
-    inspects[i] = inspectsClass[i].href;
+function start(){
+	var inspectsClass = document.getElementsByClassName("sih-market-action");
+	var pricesClass = document.getElementsByClassName("market_listing_price market_listing_price_with_fee");
+	var inspects = new Array(inspectsClass.length);
+	var prices = new Array(pricesClass.length);
+
+	var data = new Array(inspectsClass.length);
+
+	//Get Values
+	for (i = 0; i < inspectsClass.length; i++) { 
+		inspects[i] = inspectsClass[i].href;
+	}
+	for (i = 0; i < pricesClass.length; i++) { 
+		prices[i] = pricesClass[i].innerHTML.trim().replace(",", ".");
+	}
+
+	//Mount DATA
+	for(i = 0; i < inspectsClass.length; i++){
+		data[i] = inspects[i] + " " +  prices[i];
+	}
+
+	var stringData;
+	for(i = 0; i < data.length; i++){
+		stringData = stringData + data[i];
+	}
+
+	//Print data
+	var inputArea = document.createElement('p');
+	inputArea.setAttribute('type', 'text');
+	inputArea.innerHTML = data;
+	document.body.appendChild(inputArea);
 }
-for (i = 0; i < pricesClass.length; i++) { 
-    prices[i] = pricesClass[i].innerHTML.trim().replace(",", ".");
-}
-
-//Mount DATA
-for(i = 0; i < inspectsClass.length; i++){
-	data[i] = inspects[i] + " " +  prices[i];
-}
-
-var stringData;
-for(i = 0; i < data.length; i++){
-	stringData = stringData + data[i];
-}
-
-//Print data
-var inputArea = document.createElement('p');
-inputArea.setAttribute('type', 'text');
-inputArea.innerHTML = data;
-document.body.appendChild(inputArea);
