4a68b019c685d3feedd9fd0d4a1be90dababc61e
badbyte <macm241192@gmail.com>
2015-11-14 18:26:51 +0100

    sd

--- marketData.user.js
+++ marketData.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Gives the data from any csgo weapon into a string
 // @author       Baldbyte ~ Miguel200761
-// @match        http://steamcommunity.com/market/listings/730
+// @match        http://steamcommunity.com/market/listings/730/*
 // @grant        none
 // @updateURL https://raw.githubusercontent.com/miguel200761/csgoMarketCheck/master/marketData.js
 // @downloadURL https://raw.githubusercontent.com/miguel200761/csgoMarketCheck/master/marketData.js
